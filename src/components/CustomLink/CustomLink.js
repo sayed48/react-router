57e4c024e8707e3f5d51e79503d9a1293f5b7622
sayed48 <masayedmas48@gmail.com>
2022-04-02 18:33:30 +0600

    custom link add

--- src/components/CustomLink/CustomLink.js
+++ src/components/CustomLink/CustomLink.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 
-const CustomLink = () => {
+function CustomLink({ children, to, ...props }) {
+    let resolved = useResolvedPath(to);
+    let match = useMatch({ path: resolved.pathname, end: true });
+  
     return (
-        <div>
-            
-        </div>
+      <div>
+        <Link
+          style={{ color: match ? 'green': 'aqua', textDecoration: match ? "underline" : "none" }}
+          to={to}
+          {...props}
+        >
+          {children}
+        </Link>
+      </div>
     );
-};
+  }
 
 export default CustomLink;
